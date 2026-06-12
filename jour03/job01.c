96ee42aa326385e341e110e7b8256b38d191f5f4
André <gwandrewgd@gmail.com>
2026-06-12 14:47:27 +0200

    jour02

---
+++ jour03/job01.c
@@ -0,0 +1,34 @@
+int is_digit(char c)
+{
+    if (c >= '0' && c <= '9')
+        return 1;
+    return 0;
+}
+
+int is_lower(char c)
+{
+    if (c >= 'a' && c <= 'z')
+        return 1;
+    return 0;
+}
+
+int is_upper(char c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    return 0;
+}
+
+int is_alpha(char c)
+{
+    if (is_upper(c) || is_lower(c))
+        return 1;
+    return 0;
+}
+
+int is_print(char c)
+{
+    if (is_alpha(c) || is_digit(c))
+        return 1;
+    return 0;
+}
